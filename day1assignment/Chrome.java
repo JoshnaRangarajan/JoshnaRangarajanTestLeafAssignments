ba4085982efe8d923852eff908c7748ef87ac1eb
JoshnaRangarajan <79657771+JoshnaRangarajan@users.noreply.github.com>
2024-11-20 15:21:39 +0100

    Add files via upload
    day1assignment

---
+++ day1assignment/Chrome.java
@@ -0,0 +1,32 @@
+package week1.day1assignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Datatype:float
+		float version=91.0f;
+		System.out.println(version);
+		
+		//Datatype:String
+		 String developer="Google";
+		 System.out.println(developer);
+		 
+		//Datatype:boolean
+		 boolean Beta =false;
+		 System.out.println(Beta);
+		 
+		//Datatype:int
+		 int releaseYear=2008;
+		 System.out.println(releaseYear);
+		 
+		//Datatype:char
+		 char shortcutKey='C';
+		 System.out.println(shortcutKey);
+		
+		
+
+	}
+
+}
